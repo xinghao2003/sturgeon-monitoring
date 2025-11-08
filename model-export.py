c89e918f903718e931049195d6c8b13e9c607b90
Wong Xing Hao <wongxh45@gmail.com>
2025-11-09 04:04:06 +0800

    Include model files, and example video input

--- model-export.py
+++ model-export.py
@@ -1,16 +1,16 @@
 from ultralytics import YOLO
 
 # Load model
-model = YOLO(r"C:\Users\xingh\Desktop\sturgeon-monitoring-v2\models\best.pt")
+model = YOLO(r"models\best.pt")
 
 try:
-    # model.export(format="openvino")
-    # model.export(format="engine")
-    # model.export(format="onnx")
+    model.export(format="openvino")
+    model.export(format="engine")
+    model.export(format="onnx")
     model.export(format="torchscript")
-    # model.export(format="tflite")
-    # model.export(format="edgetpu")
-    # model.export(format="executorch")
+    model.export(format="tflite")
+    model.export(format="edgetpu")
+    model.export(format="executorch")
 except Exception as e:
     print(f"Export failed: {e}")
     import traceback
